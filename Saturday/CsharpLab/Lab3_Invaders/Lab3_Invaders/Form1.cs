a059a122efcc8941f32526c62e6c581259e08c3e
Niiok <play.niiok@gmail.com>
2021-10-22 21:03:43 +0900

    Update Form1.cs

--- Saturday/CsharpLab/Lab3_Invaders/Lab3_Invaders/Form1.cs
+++ Saturday/CsharpLab/Lab3_Invaders/Lab3_Invaders/Form1.cs
@@ -23,6 +23,7 @@ namespace Lab3_Invaders
         List<Keys> keysPressed = new List<Keys>();
         Game game = new Game();
         bool gameOver;
+        int animationCycle = 3;
 
         public Form1()
         {
@@ -31,21 +32,30 @@ namespace Lab3_Invaders
 
 
 
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Q)
-                Application.Exit();
-            if (gameOver)
-                if (e.KeyCode == Keys.S)
-                {
-                    // code to reset the game and restart the timers
-                    return;
-                }
-            if (e.KeyCode == Keys.Space)
-                game.FireShot();
-            if (keysPressed.Contains(e.KeyCode))
-                keysPressed.Remove(e.KeyCode);
-            keysPressed.Add(e.KeyCode);
+            switch(e.KeyCode)
+            {
+                case Keys.Q:
+                    Application.Exit();
+                    break;
+                case Keys.S:
+                    if (gameOver)
+                    {
+                        // code to reset the game and restart the timers
+                        return;
+                    }
+                    break;
+                case Keys.Space:
+                    game.FireShot();
+                    break;
+                default:
+                    if (keysPressed.Contains(e.KeyCode))
+                        keysPressed.Remove(e.KeyCode);
+                    keysPressed.Add(e.KeyCode);
+                    break;
+            }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
@@ -56,7 +66,20 @@ namespace Lab3_Invaders
 
         private void timer_animation_Tick(object sender, EventArgs e)
         {
+            switch(animationCycle)
+            {
+                case 4:  animationCycle = -1; break;
+                case 3:  animationCycle++; break;
+                case 2:  animationCycle++; break;
+                case 1:  animationCycle++; break;
+                case -1: animationCycle--; break;
+                case -2: animationCycle--; break;
+                case -3: animationCycle--; break;
+                case -4: animationCycle = 1; break;
+            }
 
+            game.Twinkle();
+            Refresh();
         }
 
         private void timer_game_Tick(object sender, EventArgs e)
@@ -64,15 +87,14 @@ namespace Lab3_Invaders
             game.Go();
             foreach (Keys key in keysPressed)
             {
-                if (key == Keys.Left)
+                switch(key)
                 {
-                    game.MovePlayer(Direction.Left);
-                    return;
-                }
-                else if (key == Keys.Right)
-                {
-                    game.MovePlayer(Direction.Right);
-                    return;
+                    case Keys.Left:
+                        game.MovePlayer(Direction.Left);
+                        return;
+                    case Keys.Right:
+                        game.MovePlayer(Direction.Right);
+                        return;
                 }
             }
         }
